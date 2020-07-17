04954d92404523ff1fa9df92196e16b67b7b13c5
jolisdegats <szwarc.julie@gmail.com>
2020-07-17 17:03:29 +0200

    form added

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+const $ = document;
+
+$.addEventListener("DOMContentLoaded", () => {
+  //   console.log("Page chargée");
+
+  $.querySelector(".close").addEventListener("click", () => {
+    $.querySelector(".visible").classList.replace("visible", "hidden");
+  });
+  $.querySelector(".popupbackground").addEventListener("click", () => {
+    $.querySelector(".visible").classList.replace("visible", "hidden");
+  });
+
+  $.querySelector(".contactOpener").addEventListener("click", () => {
+    $.querySelector(".hidden").classList.replace("hidden", "visible");
+  });
+
+  $.querySelector("#contactForm").addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const data = {
+      firstname: document.querySelector("#firstname").value,
+      lastname: document.querySelector("#lastname").value,
+      email: document.querySelector("#email").value,
+      message: document.querySelector("#message").value,
+    };
+
+    console.log(data);
+
+    const response = await axios.post(
+      "http://56c25514648c.ngrok.io/submit-form",
+      data
+    );
+    console.log(response);
+
+    if (response.status === 200) {
+      alert("Merci pour votre message ! Votre formulaire a bien été envoyé");
+    }
+
+    $.querySelector(".visible").classList.replace("visible", "hidden");
+  });
+});
